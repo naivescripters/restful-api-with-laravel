3f93c4f02c517184fefe62274ef6d6a831b731b0
Abul Hasan <abul.hasan.ict@gmail.com>
2019-09-24 09:50:14 +0600

    Crud with resource controller

--- app/Http/Controllers/Country/CountryController.php
+++ app/Http/Controllers/Country/CountryController.php
@@ -6,7 +6,7 @@ use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
 use App\Models\CountryModel;
-
+use Validator;
 class CountryController extends Controller
 {
     public function country(){
@@ -16,13 +16,24 @@ class CountryController extends Controller
     public function countryByID($id){
         $country = CountryModel::find($id);
         if(is_null($country)){
-            return response()->json("Record not Found!",404);
+            return response()->json(["message" => "Record not Found!"],404);
         }
         return response()->json($country,200);
 
     }
 
     public function countrySave(Request $request){
+        $rules = [
+            'name' => 'required|min:3',
+            'iso' => 'required|min:2|max:2',
+        ];
+
+        $validator = Validator::make($request->all(),$rules);
+        if($validator->fails()){
+            return response()->json($validator->errors(),400);
+        }
+
+
         $country = CountryModel::create($request->all());
         return response()->json($country,201);
     }
@@ -30,7 +41,7 @@ class CountryController extends Controller
     public function countryUpdate(Request $request, $id){
         $country = CountryModel::find($id);
         if(is_null($country)){
-            return response()->json("Record not Found!",404);
+            return response()->json(["message" => "Record not Found!"],404);
         }
         $country ->update($request->all());
         return response()->json($country,200);
@@ -39,7 +50,7 @@ class CountryController extends Controller
     public function countryDelete(Request $request, $id){
         $country = CountryModel::find($id);
         if(is_null($country)){
-            return response()->json("Record not Found!",404);
+            return response()->json(["message" => "Record not Found!"],404);
         }
         $country ->delete();
         return response()->json(null,204);
